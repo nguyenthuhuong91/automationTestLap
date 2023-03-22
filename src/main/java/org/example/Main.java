9d3edeace61b1739227fa01e0ea9ec8a239f447c
LAPTOP-Q1RBEFFD\Thu Huong <nguyenthuhuong.cntt@gmail.com>
2023-03-22 10:52:58 +0700

    change file manin

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,6 +5,7 @@ public class Main {
 
         System.out.println("Hello world!");
         System.out.println("Hello automation test 11111");
+        System.out.println("Hello automation test 22222");
 
     }
 
